971f6f0f27f8e9dfb0bbd9a169a4e041c623b214
VisitMoon <moungyeong@gmail.com>
2023-06-29 14:11:04 +0900

    moon

--- src/main/java/OhShu/Servlet/UserJoinServlet.java
+++ src/main/java/OhShu/Servlet/UserJoinServlet.java
@@ -37,7 +37,7 @@ public class UserJoinServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		RequestDispatcher dispatcher = request.getRequestDispatcher("view/js/signup.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/view/join.jsp");
 		dispatcher.forward(request, response);
 
 	}
